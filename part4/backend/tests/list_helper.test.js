b0cc9890b5945af3c19669917fc5131732c98c1f
yussiv <jussi.voipio@cs.helsinki.fi>
2020-05-29 01:06:48 +0300

    4.5

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -1,47 +1,50 @@
 const listHelper = require('../utils/list_helper')
 
+const emptyBlogs = []
+
+const oneBlog = [
+  {
+    "title": "The C Programming Language",
+    "author": "Brian Kernighan, Dennis Ritchie",
+    "url": "https://archive.org/details/TheCProgrammingLanguageFirstEdition",
+    "likes": 8
+  }
+]
+
+const manyBlogs = [
+  {
+    "title": "The C Programming Language",
+    "author": "Brian Kernighan, Dennis Ritchie",
+    "url": "https://archive.org/details/TheCProgrammingLanguageFirstEdition",
+    "likes": 8
+  },
+  {
+    "title": "The Go Programming Language",
+    "author": "Robert Griesemer, Rob Pike, Ken Thompson",
+    "url": "https://golang.org/",
+    "likes": 1
+  },
+  {
+    "title": "Patterns for Managing Source Code Branches",
+    "author": "Martin Fowler",
+    "url": "https://martinfowler.com/articles/branching-patterns.html",
+    "likes": 42
+  },
+  {
+    "title": "The Go Programming Language ",
+    "author": "Uncle Bob",
+    "url": "http://blog.cleancoder.com/uncle-bob/2020/05/27/ReplDrivenDesign.html",
+    "likes": 7
+  },
+  {
+    "title": "KeystoneInterface",
+    "author": "Martin Fowler",
+    "url": "https://martinfowler.com/bliki/KeystoneInterface.html",
+    "likes": 2
+  }
+]
+
 describe('total likes', () => {
-
-  const emptyBlogs = []
-
-  const oneBlog = [
-    {
-      "_id": "5ed0160baa9badeb17b6f585",
-      "title": "The C Programming Language",
-      "author": "Brian Kernighan, Dennis Ritchie",
-      "url": "https://archive.org/details/TheCProgrammingLanguageFirstEdition",
-      "likes": 42,
-      "__v": 0
-    }
-  ]
-
-  const threeBlogs = [
-    {
-      "_id": "5ed0160baa9badeb17b6f585",
-      "title": "The C Programming Language",
-      "author": "Brian Kernighan, Dennis Ritchie",
-      "url": "https://archive.org/details/TheCProgrammingLanguageFirstEdition",
-      "likes": 42,
-      "__v": 0
-    },
-    {
-      "_id": "5ed01692aa9badeb17b6f586",
-      "title": "The C++ Programming Language",
-      "author": "Bjarne Stroustrup",
-      "url": "http://www.stroustrup.com/books.html",
-      "likes": 8,
-      "__v": 0
-    },
-    {
-      "_id": "5ed01d93b7dc40f26a6ef17a",
-      "title": "The Go Programming Language ",
-      "author": "Robert Griesemer, Rob Pike, Ken Thompson",
-      "url": "https://golang.org/",
-      "likes": 5,
-      "__v": 0
-    }
-  ]
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes(emptyBlogs)
     expect(result).toBe(0)
@@ -49,11 +52,38 @@ describe('total likes', () => {
 
   test('of one blog is correct', () => {
     const result = listHelper.totalLikes(oneBlog)
-    expect(result).toBe(42)
+    expect(result).toBe(8)
   })
 
   test('of three blogs is correct', () => {
-    const result = listHelper.totalLikes(threeBlogs)
-    expect(result).toBe(55)
+    const result = listHelper.totalLikes(manyBlogs)
+    expect(result).toBe(60)
+  })
+})
+
+describe('favourite blog', () => {
+  test('of empty list is empty object', () => {
+    const result = listHelper.favoriteBlog(emptyBlogs)
+    expect(result).toEqual({})
+  })
+
+  test('of one blog is the only blog', () => {
+    const result = listHelper.favoriteBlog(oneBlog)
+    expect(result).toEqual({
+      "title": "The C Programming Language",
+      "author": "Brian Kernighan, Dennis Ritchie",
+      "url": "https://archive.org/details/TheCProgrammingLanguageFirstEdition",
+      "likes": 8
+    })
+  })
+
+  test('of many blogs is correct', () => {
+    const result = listHelper.favoriteBlog(manyBlogs)
+    expect(result).toEqual({
+      "title": "Patterns for Managing Source Code Branches",
+      "author": "Martin Fowler",
+      "url": "https://martinfowler.com/articles/branching-patterns.html",
+      "likes": 42
+    })
   })
 })
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => {
+    if (!fav.likes || fav.likes < blog.likes)
+     return blog
+    else
+      return fav
+  }, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
